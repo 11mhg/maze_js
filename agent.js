841fd03a283ddf52bfefc5b907c88a3efe2f73fe
Mohammed Hamada Gasmallah <11mhg@queensu.ca>
2019-09-01 01:22:03 -0400

    Fixed memory and better agent AI learning

--- agent.js
+++ agent.js
@@ -1,31 +1,36 @@
-class Agent{
+class Agent {
 
-    constructor(state_width=10,state_height=10,maze){
-        this.state_width=state_width;
-        this.state_height=state_height;
+    constructor(state_width = 10, state_height = 10, maze) {
+        this.state_width = state_width;
+        this.state_height = state_height;
         this.state = maze;
-        this.actions = [[1,0],[0,1],[-1,0],[0,-1]]; //left right up down (remember y is upside down)
+        this.actions = [
+            [1, 0],
+            [0, 1],
+            [-1, 0],
+            [0, -1]
+        ]; //left right up down (remember y is upside down)
         this.x = 0;
         this.y = 0;
         this.init_state();
-        this.model = new Model(this,[this.state_width,this.state_height],this.actions.length);
+        this.model = new Model(this, [this.state_width, this.state_height], this.actions.length);
     }
 
-    hasChanged(){
+    hasChanged() {
         return ((this.prev_x != this.x) && (this.prev_y != this.y));
     }
 
-    init_state(){
-        let start_x = Math.round(Math.random()*this.state_width-2) + 1;
-        let start_y = Math.round(Math.random()*this.state_height-2) + 1;
-        
-        while(this.state.maze.get(start_x,start_y)==1){
-            let dir_x = Math.round(Math.random()*2) - 1;
-            let dir_y = Math.round(Math.random()*2) - 1;
-            if ((start_x+dir_x > 0) && (start_x+dir_x < this.state_width-1)){
+    init_state() {
+        let start_x = Math.round(Math.random() * this.state_width - 2) + 1;
+        let start_y = Math.round(Math.random() * this.state_height - 2) + 1;
+
+        while (this.state.maze.get(start_x, start_y) == 1) {
+            let dir_x = Math.round(Math.random() * 2) - 1;
+            let dir_y = Math.round(Math.random() * 2) - 1;
+            if ((start_x + dir_x > 0) && (start_x + dir_x < this.state_width - 1)) {
                 start_x += dir_x;
             }
-            if ((start_y+dir_y > 0) && (start_y+dir_y < this.state_height-1)){
+            if ((start_y + dir_y > 0) && (start_y + dir_y < this.state_height - 1)) {
                 start_y += dir_y;
             }
         }
@@ -37,25 +42,26 @@ class Agent{
         this.prev_y = this.y;
     }
 
-    update(action){
+    update(action_val) {
+        let action = this.actions[action_val];
         this.prev_x = this.x;
         this.prev_y = this.y;
         let temp_x = this.x + action[0];
         let temp_y = this.y + action[1];
 
-        if (this.state.maze.get(temp_x,temp_y)!=0){
-            if (this.state.maze.get(temp_x,this.y)!=0){
-                if (this.state.maze.get(this.x,temp_y)!=0){
+        if (this.state.maze.get(temp_x, temp_y) != 0) {
+            if (this.state.maze.get(temp_x, this.y) != 0) {
+                if (this.state.maze.get(this.x, temp_y) != 0) {
                     temp_x = this.x;
                     temp_y = this.y;
-                }else{
+                } else {
                     temp_x = this.x;
                 }
-            }else{
+            } else {
                 temp_y = this.y;
             }
         }
-        if (this.state.maze.get(temp_x,temp_y)!=1){
+        if (this.state.maze.get(temp_x, temp_y) != 1) {
             this.x = temp_x;
             this.y = temp_y;
         }
@@ -65,18 +71,18 @@ class Agent{
 
     //0 if we are at the goal
     //>1 if we are not.
-    distance(){
+    distance() {
         let dist = Math.abs(this.x - this.state.end_x) + Math.abs(this.y - this.state.end_y);
         return dist;
     }
 
-    isdone(){
-        return this.distance()==0;
+    isdone() {
+        return this.distance() == 0;
     }
 
-    draw(block_radius=10,agent_color=color('magenta')){
+    draw(block_radius = 10, agent_color = color('magenta')) {
         fill(agent_color);
-        rect(this.x*block_radius,this.y*block_radius,block_radius,block_radius);
+        rect(this.x * block_radius, this.y * block_radius, block_radius, block_radius);
         fill(color('black'));
     }
 
--- model.js
+++ model.js
@@ -1,38 +1,102 @@
-class Model{
+class Model {
 
-    constructor(agent,input_shape=[10,10],actions=4){
+    constructor(agent, input_shape = [10, 10], actions = 4) {
         this.agent = agent;
-        const inp = tf.input({shape: [input_shape[0],input_shape[1],1]});
-        const c1 = tf.layers.conv2d({filters:32,kernelSize:[3,3],
-            strides:1,padding:'same'});
+        const inp = tf.input({ shape: [input_shape[0], input_shape[1], 1] });
+        const c1 = tf.layers.conv2d({
+            filters: 32,
+            kernelSize: [3, 3],
+            strides: 1,
+            padding: 'same'
+        });
         const f1 = tf.layers.flatten();
-        const o1 = tf.layers.dense({units:actions});
-        console.log(o1);
+        const o1 = tf.layers.dense({ units: actions });
 
         const out = o1.apply(f1.apply(c1.apply(inp)));
-        this.model = tf.model({inputs: inp, outputs: out});
+        this.model = tf.model({ inputs: inp, outputs: out });
 
+        this.actions = actions;
         this.y = .99;
         this.e = 0.1;
         this.num_episodes = 2000;
         this.jList = [];
         this.rList = [];
+
+        this.optimizer = tf.train.sgd(0.1);
     }
 
-    trainstep(){
+    async optim(s, targetQ) {
+        let loss = (pred, label) => pred.sub(label).square().sum();
+        this.optimizer.minimize(() => loss(this.model.predict(s), targetQ));
+    }
+
+    async trainstep(count = 0) {
         this.agent.init_state();
         let rAll = 0;
-        let d=false;
         let j = 0;
-        while (j < 99){
-            j+=1;
-            let in_s = tf.tensord2d(this.agent.state.maze.tolist()).expandDims(-1).expandDims(0);
-            let out = this.model.predict(in_s);
-            
-            console.log(in_s);
-            
+        while (j < 99) {
+            j += 1;
+
+            let s = tf.tidy(() => { return tf.tensor2d(this.agent.state.maze.tolist()).expandDims(-1).expandDims(0) });
+            let allQ = this.model.predict(s);
+            let a = tf.argMax(allQ, -1);
+
+            if (Math.random() < this.e) {
+                tf.dispose({
+                    a
+                })
+                a = tf.tensor([
+                    [Math.round(Math.random() * (this.actions - 1))]
+                ]);
+            }
+            let prev_dist = this.agent.distance();
+            let a_val = await a.dataSync()[0];
+            let dist = this.agent.update(a_val);
+            let s1 = tf.tidy(() => { return tf.tensor2d(this.agent.state.maze.tolist()).expandDims(-1).expandDims(0) });
+            let r = -1;
+            if (prev_dist > dist) {
+                r = 1;
+            } else if (prev_dist == dist) {
+                r = 0;
+            }
+
+            let Q1 = this.model.predict(s1);
+            let maxQ1 = Q1.max();
+            let maxQ1_val = await maxQ1.dataSync()[0];
+
+            let targetQ = await allQ.dataSync();
+            targetQ[a_val] = r + this.y * maxQ1_val;
+            targetQ = tf.tensor2d([targetQ]);
+            await this.optim(s, targetQ);
+            rAll += r;
+
+            tf.dispose({
+                s,
+                allQ,
+                a,
+                s1,
+                Q1,
+                maxQ1,
+                targetQ
+            })
+
+            if (dist == 0) {
+                e = 1. / ((count / 50) + 10);
+                break;
+            }
+            await tf.nextFrame();
         }
+        this.jList.push(j);
+        this.rList.push(rAll);
+        return rAll / j;
     }
 
+    async train(num_episodes = 10) {
+        for (let i = 0; i < num_episodes; i++) {
+            await this.trainstep(i);
+            console.log(tf.memory().numTensors);
+        }
+        return nj.array(this.rList).sum() / num_episodes;
+    }
 
 }
